bc0b66797c50f6dfe1beef388e7ee0f3e2cdd00c
yannik <yannik@crafted-code.org>
2026-05-14 11:48:03 +0200

    feat: Enhance Project and Task specifications with additional filters for status, organization, priority, due date, and project ID

--- src/main/java/org/craftedcode/backend/repository/specification/ProjectSpecification.java
+++ src/main/java/org/craftedcode/backend/repository/specification/ProjectSpecification.java
@@ -1,5 +1,15 @@
 package org.craftedcode.backend.repository.specification;
 
+import de.frachtwerk.essencium.backend.repository.specification.BaseModelSpec;
+import net.kaczmarzyk.spring.data.jpa.domain.In;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.craftedcode.backend.model.Project;
 
-public interface ProjectSpecification extends AbstractModelSpec<Project> {}
+@Spec(path = "status", params = "status", spec = In.class, paramSeparator = ',')
+interface ProjectStatusSpecification extends BaseModelSpec<Project, Long> {}
+
+@Spec(path = "organization.id", params = "organizationId", spec = In.class, paramSeparator = ',')
+interface ProjectOrganizationSpecification extends BaseModelSpec<Project, Long> {}
+
+public interface ProjectSpecification
+    extends ProjectStatusSpecification, ProjectOrganizationSpecification {}
--- src/main/java/org/craftedcode/backend/repository/specification/TaskSpecification.java
+++ src/main/java/org/craftedcode/backend/repository/specification/TaskSpecification.java
@@ -1,5 +1,25 @@
 package org.craftedcode.backend.repository.specification;
 
+import de.frachtwerk.essencium.backend.repository.specification.BaseModelSpec;
+import net.kaczmarzyk.spring.data.jpa.domain.In;
+import net.kaczmarzyk.spring.data.jpa.domain.LessThanOrEqual;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.craftedcode.backend.model.Task;
 
-public interface TaskSpecification extends AbstractModelSpec<Task> {}
+@Spec(path = "status", params = "status", spec = In.class, paramSeparator = ',')
+interface TaskStatusSpecification extends BaseModelSpec<Task, Long> {}
+
+@Spec(path = "priority", params = "priority", spec = In.class, paramSeparator = ',')
+interface TaskPrioritySpecification extends BaseModelSpec<Task, Long> {}
+
+@Spec(path = "dueDate", params = "dueUntil", spec = LessThanOrEqual.class, paramSeparator = ',')
+interface TaskDueDateSpecification extends BaseModelSpec<Task, Long> {}
+
+@Spec(path = "project.id", params = "projectId", spec = In.class, paramSeparator = ',')
+interface TaskProjectSpecification extends BaseModelSpec<Task, Long> {}
+
+public interface TaskSpecification
+    extends TaskStatusSpecification,
+        TaskPrioritySpecification,
+        TaskDueDateSpecification,
+        TaskProjectSpecification {}
